96466971490c73e7ee84607260e9511db78eea7c
Niglou <galaxneil.super@gmail.com>
2020-03-18 21:19:00 +0100

    update init

--- components/src/init.cpp
+++ components/src/init.cpp
@@ -30,25 +30,25 @@ void disable_procpu_wdt_flashboot() {
 
 void init_esp32() {
 
-    disable_rtc_wdt();
+  disable_rtc_wdt();
 
-    disable_procpu_wdt_flashboot();
+  disable_procpu_wdt_flashboot();
 
-    /* Set CPU on PLL clock */
-    /* 0: XTAL, 1: PLL, 2: CK8M, 3: APLL */
-    RTCCNTL.clk_conf.soc_clk_sel = 1;
-    /* CPU Freq 0:80Mhz 1:160Mhz 2:240Mhz */
-    //((unsigned int*)DPORT_CPU_PER_CONF_REG) = 0;
+  /* Set CPU on PLL clock */
+  /* 0: XTAL, 1: PLL, 2: CK8M, 3: APLL */
+  RTCCNTL.clk_conf.soc_clk_sel = 1;
+  /* CPU Freq 0:80Mhz 1:160Mhz 2:240Mhz */
+  //((unsigned int*)DPORT_CPU_PER_CONF_REG) = 0;
 
 
-	// Move exception vectors to IRAM
-	asm volatile (
-                  "wsr    %0, vecbase\n" \
-                  ::"r"(&_init_start));
+  // Move exception vectors to IRAM
+  asm volatile (
+    "wsr    %0, vecbase\n" \
+    ::"r"(&_init_start));
 
-	/* UART divider to 694(80 000 000/115 200) */
-  /* UART divider to 138(80 000 000/576 000) */
-  /* UART divider to 87 (80 000 000/921 600) */
+  /* UART divider to 694 (80 000 000/115 200) */
+  /* UART divider to 138 (80 000 000/576 000) */
+  /* UART divider to 87  (80 000 000/921 600) */
   UART0.clk_div.div_int = 694;
   //UART0.conf0.tick_ref_always_on = 0;
 
