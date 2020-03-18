5a13f5714cc048c1748e5bca0b5a30ec53ffa8ff
Niglou <galaxneil.super@gmail.com>
2020-03-18 21:18:42 +0100

    add interrupt

--- components/src/intr.cpp
+++ components/src/intr.cpp
@@ -1,5 +1,19 @@
 #include "intr.h"
 
+extern "C" {
+
+  void ISR_LVL2() __attribute__((weak));
+  void ISR_LVL3() __attribute__((weak));
+  void ISR_LVL4() __attribute__((weak));
+  void ISR_LVL5() __attribute__((weak));
+
+}
+
+void ISR_LVL2() {}
+void ISR_LVL3() {}
+void ISR_LVL4() {}
+void ISR_LVL5() {}
+
 void intr_enable(unsigned int interrupt) {
     asm volatile (
 		"wsr %0, intenable \n"
